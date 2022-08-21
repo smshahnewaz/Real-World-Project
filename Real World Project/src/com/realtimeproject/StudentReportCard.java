fef04706c587e541c9646a0e26560a1c59c3d2c6
SM Shahnewaz <shahnewazsm@gmail.com>
2022-08-21 10:48:28 -0400

    First Commit

---
+++ Real World Project/src/com/realtimeproject/StudentReportCard.java
@@ -0,0 +1,19 @@
+package com.realtimeproject;
+import java.util.Arrays;
+
+public class StudentReportCard {
+	public static void main(String[] args) {
+		boolean [] arr1= {false, true, false};
+		int [] arr2= {1,2,3,4,5,6,7,8,9,0};
+		double [] arr3= {0.110,399.0,332,0.123};
+		/*
+		 * System.out.println(Arrays.toString(arr1));
+		 * System.out.println(Arrays.toString(arr2));
+		 * System.out.println(Arrays.toString(arr3));
+		 */
+		for(int i=0; i<=1100; i++) {
+			System.out.println(" "+arr2[i]+" "+arr3[i]);
+		}
+	}
+
+}
